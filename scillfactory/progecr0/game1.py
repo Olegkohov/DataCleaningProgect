00f05d1a568cfe1cd535bd5a33d5758f7990df60
olegkohov <kohovleg@mail.ru>
2022-02-07 17:31:14 +0300

    https://github.com/Olegkohov/urban-chainsaw

--- scillfactory/progecr0/game1.py
+++ scillfactory/progecr0/game1.py
@@ -3,4 +3,37 @@
 import numpy as np
 
 def random_predict(number: int = 1) -> int:
-    number = np.random.randint(1, 101) # загадываем число
+    cislo = np.random.randint(1, 101)
+    count = 0
+    print(cislo)
+    if number > cislo:
+        count += 1
+        while number > cislo:
+            number -= 10
+            count += 1
+            if number < cislo:
+                while True:
+                    number += 1
+                    count += 1
+                    if number == cislo:
+                        return count
+            if number == cislo:
+                return count
+    elif number < cislo:
+        count += 1
+        while number < cislo:
+            number += 10
+            count += 1
+            if number > cislo:
+                while True:
+                    count += 1
+                    number -= 1
+                    if number == cislo:
+                        return count 
+            if number == cislo:
+                return count
+                         
+                    
+    else:
+        return 1
+print(random_predict(1))
