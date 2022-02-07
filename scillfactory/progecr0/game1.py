85626c6db347aba57ea6dd4cef7a68579d1776f4
olegkohov <kohovleg@mail.ru>
2022-02-07 17:55:34 +0300

    https://github.com/Olegkohov/urban-chainsaw

--- scillfactory/progecr0/game1.py
+++ scillfactory/progecr0/game1.py
@@ -4,14 +4,14 @@ import numpy as np
 
 def random_predict(number: int = 1) -> int:
     cislo = np.random.randint(1, 101)
-    count = 0
+    count = 0 # количество попыток
     print(cislo)
-    if number > cislo:
+    if number > cislo: #Если наше число больше заданного то мы уменьшаем его на 10 до тех пор пока оно не станет меньше.
         count += 1
         while number > cislo:
             number -= 10
             count += 1
-            if number < cislo:
+            if number < cislo: #После этого прибавляем 1 пока наше число не станет равно загаданному
                 while True:
                     number += 1
                     count += 1
@@ -19,7 +19,7 @@ def random_predict(number: int = 1) -> int:
                         return count
             if number == cislo:
                 return count
-    elif number < cislo:
+    elif number < cislo:# Аналогично. Только с нашем числом действия меняются на противоположные 
         count += 1
         while number < cislo:
             number += 10
@@ -36,4 +36,4 @@ def random_predict(number: int = 1) -> int:
                     
     else:
         return 1
-print(random_predict(1))
+print(random_predict(1)) #Сначала на экране появляется число загаданное компьютором потом количество попыток
